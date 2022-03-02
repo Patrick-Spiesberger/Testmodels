ca3a0d739f739722b86489cf70cfcf840da651fb
Patrick Spiesberger <53953417+Patrick-Spiesberger@users.noreply.github.com>
2022-03-02 18:51:09 +0100

    added ListEffort models and test | see description
    please do not run SimpleListEffortTwentyFiveTest => stress test

---
+++ edu.kit.ipd.sdq.kamp4attack.tests/src/edu/kit/ipd/sdq/kamp4attack/tests/mitigation/extension/effort/SimpleListEffortOneTest.java
@@ -0,0 +1,40 @@
+package edu.kit.ipd.sdq.kamp4attack.tests.mitigation.extension.effort;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import edu.kit.ipd.sdq.kamp4attack.core.AttackPropagationAnalysis;
+import edu.kit.ipd.sdq.kamp4attack.model.modificationmarks.KAMP4attackModificationmarks.CredentialChange;
+import edu.kit.ipd.sdq.kamp4attack.tests.AbstractModelTest;
+
+class SimpleListEffortOneTest extends AbstractModelTest {
+
+	public SimpleListEffortOneTest() {
+		this.PATH_ATTACKER = "contextSelectionModels/effortModels/simpleEffortModel/attacker/oneAttribute.attacker";
+		this.PATH_ASSEMBLY = "contextSelectionModels/effortModels/simpleEffortModel/simpleEffort.system";
+		this.PATH_ALLOCATION = "contextSelectionModels/effortModels/simpleEffortModel/simpleEffort.allocation";
+		this.PATH_CONTEXT = "contextSelectionModels/effortModels/simpleEffortModel/effort.context";
+		this.PATH_MODIFICATION = "contextSelectionModels/effortModels/simpleEffortModel/simpleEffort.kamp4attackmodificationmarks";
+		this.PATH_REPOSITORY = "contextSelectionModels/effortModels/simpleEffortModel/simpleEffort.repository";
+		this.PATH_USAGE = "contextSelectionModels/effortModels/simpleEffortModel/simpleEffort.usagemodel";
+		this.PATH_RESOURCES = "contextSelectionModels/effortModels/simpleEffortModel/simpleEffort.resourceenvironment";
+	}
+
+	@BeforeEach
+	protected void execute() {
+		generateXML();
+		final var wrapper = getBlackboardWrapper();
+		(new AttackPropagationAnalysis()).runChangePropagationAnalysis(wrapper);
+	}
+
+	@Test
+	void testSimpleCaseOneAttribute() {
+
+		final var steps = this.modification.getChangePropagationSteps();
+
+		assertEquals(1, steps.size());
+		assertEquals(1, ((CredentialChange) steps.get(0)).getCompromisedassembly().size());
+	}
+}
---
+++ edu.kit.ipd.sdq.kamp4attack.tests/src/edu/kit/ipd/sdq/kamp4attack/tests/mitigation/extension/effort/SimpleListEffortSevenTest.java
@@ -0,0 +1,39 @@
+package edu.kit.ipd.sdq.kamp4attack.tests.mitigation.extension.effort;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import edu.kit.ipd.sdq.kamp4attack.core.AttackPropagationAnalysis;
+import edu.kit.ipd.sdq.kamp4attack.model.modificationmarks.KAMP4attackModificationmarks.CredentialChange;
+import edu.kit.ipd.sdq.kamp4attack.tests.AbstractModelTest;
+
+class SimpleListEffortSevenTest extends AbstractModelTest {
+
+	public SimpleListEffortSevenTest() {
+		this.PATH_ATTACKER = "contextSelectionModels/effortModels/simpleEffortModel/attacker/sevenAttribute.attacker";
+		this.PATH_ASSEMBLY = "contextSelectionModels/effortModels/simpleEffortModel/simpleEffort.system";
+		this.PATH_ALLOCATION = "contextSelectionModels/effortModels/simpleEffortModel/simpleEffort.allocation";
+		this.PATH_CONTEXT = "contextSelectionModels/effortModels/simpleEffortModel/effort.context";
+		this.PATH_MODIFICATION = "contextSelectionModels/effortModels/simpleEffortModel/simpleEffort.kamp4attackmodificationmarks";
+		this.PATH_REPOSITORY = "contextSelectionModels/effortModels/simpleEffortModel/simpleEffort.repository";
+		this.PATH_USAGE = "contextSelectionModels/effortModels/simpleEffortModel/simpleEffort.usagemodel";
+		this.PATH_RESOURCES = "contextSelectionModels/effortModels/simpleEffortModel/simpleEffort.resourceenvironment";
+	}
+
+	@BeforeEach
+	protected void execute() {
+		generateXML();
+		final var wrapper = getBlackboardWrapper();
+		(new AttackPropagationAnalysis()).runChangePropagationAnalysis(wrapper);
+	}
+
+	@Test
+	void testSimpleCaseSevenAttribute() {
+		final var steps = this.modification.getChangePropagationSteps();
+
+		assertEquals(1, steps.size());
+		assertEquals(1, ((CredentialChange) steps.get(0)).getCompromisedassembly().size());
+	}
+}
